2dcd2b3faebd06d23abcb292d89521a7468c6812
germiiii <germi560@gmail.com>
2023-11-16 12:39:18 -0300

    finish

--- Client/src/app/Components/MainLanding.jsx
+++ Client/src/app/Components/MainLanding.jsx
@@ -38,8 +38,7 @@ const MainLanding = () => {
               width={50} 
               height={50}
               className=" position-fixed" />
-            </button>
-            <Link href='walkerform'>
+            </button
             <button className='px-12 y-5 rounded-full w-full sm:w-fit bg-gradient-to-r from-blue-400 via-blue-600 to-blue-900 hover:bg-slate-800 text-white border mt-3 lg:mt-0'>
             <Image 
               src="/walker.png" 
@@ -48,7 +47,6 @@ const MainLanding = () => {
               height={50}
               className="position-fixed" />
             </button>
-            </Link>
         </div>
       </div>
     </section>
