230d014b5407f34772d41f421ba8bbab14ff7da2
germiiii <germi560@gmail.com>
2023-11-17 16:24:23 -0300

    fix the dates

--- Client/src/app/Components/WalkerRegister.jsx
+++ Client/src/app/Components/WalkerRegister.jsx
@@ -3,11 +3,9 @@ import React, { useState } from 'react';
 const WalkerRegister = () => {
   const [formData, setFormData] = useState({
     dog_capacity: 0,
+    selectedDays: [],
     start_time: '',
     end_time: '',
-    start_date: '',
-    end_date: '',
-    days_available: [],
   });
 
   const [error, setError] = useState('');
@@ -30,49 +28,33 @@ const WalkerRegister = () => {
     validateDateAndTime();
   };
 
-  const handleDaysChange = (e) => {
-    const { options } = e.target;
-    const selectedDays = Array.from(options)
-      .filter((option) => option.selected)
-      .map((option) => option.value);
+  const handleDayChange = (e) => {
+    const { value, checked } = e.target;
 
-    setFormData({
-      ...formData,
-      days_available: selectedDays,
+    setFormData((prevFormData) => {
+      if (checked) {
+        return {
+          ...prevFormData,
+          selectedDays: [...prevFormData.selectedDays, value],
+        };
+      } else {
+        return {
+          ...prevFormData,
+          selectedDays: prevFormData.selectedDays.filter((day) => day !== value),
+        };
+      }
     });
-  };
-
-  const handleDateChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-
-    validateDateAndTime();
   };
 
   const validateDateAndTime = () => {
-    if (!isStartDateBeforeEndDate()) {
-      setError('La fecha de inicio y hora no puede ser posterior a la fecha de finalización y hora.');
-    } else {
-      setError('');
-    }
-  };
-
-  const isStartDateBeforeEndDate = () => {
-    const startDate = new Date(formData.start_date + 'T' + formData.start_time);
-    const endDate = new Date(formData.end_date + 'T' + formData.end_time);
-    return startDate < endDate;
+    // Validation logic if needed
+    setError('');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!isStartDateBeforeEndDate()) {
-      setError('La fecha de inicio y hora no puede ser posterior a la fecha de finalización y hora.');
-      return;
-    }
+    // Validation logic if needed
 
     // reset error state
     setError('');
@@ -80,6 +62,7 @@ const WalkerRegister = () => {
     // ready to send to the backend
     console.log(formData);
   };
+
   return (
     <div className="container mx-auto my-10 max-w-lg">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
@@ -121,27 +104,80 @@ const WalkerRegister = () => {
           </div>
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date_range">
-            Rango de fechas disponible (¿Qué días estás disponible para pasear perros?)
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Días disponibles para pasear perros
           </label>
           <div className="flex">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="start_date"
-              type="date"
-              name="start_date"
-              value={formData.start_date}
-              onChange={handleDateChange}
-            />
-            <span className="mx-2">to</span>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="end_date"
-              type="date"
-              name="end_date"
-              value={formData.end_date}
-              onChange={handleDateChange}
-            />
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="monday"
+                value="monday"
+                checked={formData.selectedDays.includes('monday')}
+                onChange={handleDayChange}
+              />
+              Lunes
+            </label>
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="tuesday"
+                value="tuesday"
+                checked={formData.selectedDays.includes('tuesday')}
+                onChange={handleDayChange}
+              />
+              Martes
+            </label>
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="wednesday"
+                value="wednesday"
+                checked={formData.selectedDays.includes('wednesday')}
+                onChange={handleDayChange}
+              />
+              Miércoles
+            </label>
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="thursday"
+                value="thursday"
+                checked={formData.selectedDays.includes('thursday')}
+                onChange={handleDayChange}
+              />
+              Jueves
+            </label>
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="friday"
+                value="friday"
+                checked={formData.selectedDays.includes('friday')}
+                onChange={handleDayChange}
+              />
+              Viernes
+            </label>
+            <label className="block mr-4">
+              <input
+                type="checkbox"
+                name="saturday"
+                value="saturday"
+                checked={formData.selectedDays.includes('saturday')}
+                onChange={handleDayChange}
+              />
+              Sábado
+            </label>
+            <label className="block">
+              <input
+                type="checkbox"
+                name="sunday"
+                value="sunday"
+                checked={formData.selectedDays.includes('sunday')}
+                onChange={handleDayChange}
+              />
+              Domingo
+            </label>
           </div>
         </div>
 
