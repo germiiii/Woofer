a0487faf244ee90399c8fb4a3d8541ff61d24acf
germiiii <germi560@gmail.com>
2023-11-17 10:16:08 -0300

    comments a route

--- Server/Routes/userRouter.js
+++ Server/Routes/userRouter.js
@@ -5,8 +5,6 @@ const { userHandlerRegister }= require('./Handlers/userHandlerRegister')
 
 //const { userHandlerChangePassword } = require ('./Handlers/passwordHandlerChange')
 const { userGetAllHandler } = require ('./Handlers/userGetHandler')
-=======
->>>>>>> dab60942df25c90a6423c0ba4a0d1db091195e6c
 
 const userRouter = Router();
 
