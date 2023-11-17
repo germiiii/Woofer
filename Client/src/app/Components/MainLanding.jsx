e55a47395d94e71ee67c5357b997e0d2b9c7a626
germiiii <germi560@gmail.com>
2023-11-17 16:37:39 -0300

    fix thingis

--- Client/src/app/Components/MainLanding.jsx
+++ Client/src/app/Components/MainLanding.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 
 
 
+
 const MainLanding = () => {
   return (
     <section className="flex flex-col items-center justify-center h-screen">
@@ -39,6 +40,7 @@ const MainLanding = () => {
               height={50}
               className=" position-fixed" />
             </button>
+
             <button className='px-12 y-5 rounded-full w-full sm:w-fit bg-gradient-to-r from-blue-400 via-blue-600 to-blue-900 hover:bg-slate-800 text-white border mt-3 lg:mt-0'>
             <Image 
               src="/walker.png" 
--- Client/src/app/Components/WalkerRegister.jsx
+++ Client/src/app/Components/WalkerRegister.jsx
@@ -64,135 +64,89 @@ const WalkerRegister = () => {
   };
 
   return (
-    <div className="container mx-auto my-10 max-w-lg">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="dog_capacity">
-            Capacidad de perros (¿Cuántos perros puedes pasear a la vez?)
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="dog_capacity"
-            type="number"
-            name="dog_capacity"
-            value={formData.dog_capacity}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="hours_range">
-            Horario disponible (¿A qué horas estás disponible para pasear perros?)
-          </label>
-          <div className="flex">
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="dog_capacity"
+            >
+              Capacidad de perros (¿Cuántos perros puedes pasear a la vez?)
+            </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="start_time"
-              type="time"
-              name="start_time"
-              value={formData.start_time}
-              onChange={handleTimeChange}
-            />
-            <span className="mx-2">to</span>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="end_time"
-              type="time"
-              name="end_time"
-              value={formData.end_time}
-              onChange={handleTimeChange}
+              id="dog_capacity"
+              type="number"
+              name="dog_capacity"
+              value={formData.dog_capacity}
+              onChange={handleChange}
             />
           </div>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Días disponibles para pasear perros
-          </label>
-          <div className="flex">
-            <label className="block mr-4">
-              <input
-                type="checkbox"
-                name="monday"
-                value="monday"
-                checked={formData.selectedDays.includes('monday')}
-                onChange={handleDayChange}
-              />
-              Lunes
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="hours_range"
+            >
+              Horario disponible (¿A qué horas estás disponible para pasear perros?)
             </label>
-            <label className="block mr-4">
+            <div className="flex">
               <input
-                type="checkbox"
-                name="tuesday"
-                value="tuesday"
-                checked={formData.selectedDays.includes('tuesday')}
-                onChange={handleDayChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="start_time"
+                type="time"
+                name="start_time"
+                value={formData.start_time}
+                onChange={handleTimeChange}
               />
-              Martes
-            </label>
-            <label className="block mr-4">
+              <span className="mx-2 text-gray-600">a</span>
               <input
-                type="checkbox"
-                name="wednesday"
-                value="wednesday"
-                checked={formData.selectedDays.includes('wednesday')}
-                onChange={handleDayChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="end_time"
+                type="time"
+                name="end_time"
+                value={formData.end_time}
+                onChange={handleTimeChange}
               />
-              Miércoles
-            </label>
-            <label className="block mr-4">
-              <input
-                type="checkbox"
-                name="thursday"
-                value="thursday"
-                checked={formData.selectedDays.includes('thursday')}
-                onChange={handleDayChange}
-              />
-              Jueves
-            </label>
-            <label className="block mr-4">
-              <input
-                type="checkbox"
-                name="friday"
-                value="friday"
-                checked={formData.selectedDays.includes('friday')}
-                onChange={handleDayChange}
-              />
-              Viernes
-            </label>
-            <label className="block mr-4">
-              <input
-                type="checkbox"
-                name="saturday"
-                value="saturday"
-                checked={formData.selectedDays.includes('saturday')}
-                onChange={handleDayChange}
-              />
-              Sábado
-            </label>
-            <label className="block">
-              <input
-                type="checkbox"
-                name="sunday"
-                value="sunday"
-                checked={formData.selectedDays.includes('sunday')}
-                onChange={handleDayChange}
-              />
-              Domingo
-            </label>
+            </div>
           </div>
-        </div>
-
-        {/* Display error message if validation fails */}
-        {error && <p className="text-red-500 text-xs italic mb-4">{error}</p>}
-
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Registrarse
-          </button>
-        </div>
-      </form>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Días disponibles para pasear perros
+            </label>
+            <div className="grid grid-cols-2 gap-4">
+              {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].map((day) => (
+                <label key={day} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name={day}
+                    value={day}
+                    checked={formData.selectedDays.includes(day)}
+                    onChange={handleDayChange}
+                    className="mr-2 text-blue-500"
+                  />
+                  {day.charAt(0).toUpperCase() + day.slice(1)}
+                </label>
+              ))}
+            </div>
+          </div>
+          {/* Display error message if validation fails */}
+          {error && (
+            <p className="text-red-500 text-xs italic mb-4">{error}</p>
+          )}
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Registrarse
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
