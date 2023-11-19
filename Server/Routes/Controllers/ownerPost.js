32eb6a969f8b575e84a8039aef883bf6e6b48cf3
RRodriguezViegas <ramiirv26@gmail.com>
2023-11-19 11:42:03 -0300

    cloudinary start

--- Server/Routes/Controllers/ownerPost.js
+++ Server/Routes/Controllers/ownerPost.js
@@ -1,4 +1,30 @@
 const { User, Owner, Dog } = require("../../Database/db");
+const cloudinary = require("cloudinary").v2;
+const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+// upload image function
+const uploadImage = async imagePath => {
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
+
+  try {
+    const result = await cloudinary.uploader.upload(imagePath, options);
+    console.log(result);
+    return result.public_id;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const ownerPost = async (username, dogs) => {
   // validations
