39e849ebc1f2262ec253f8b222a15b4e3574b832
Esteban <teten08@hotmail.com>
2023-11-19 20:40:54 -0300

    Commit pedido

---
+++ Client/src/app/Components/ForgotPassword.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/reset-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Recover account</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          E-mail addres:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <button type="submit">Send Form</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default ForgotPassword;
--- Client/src/app/forget-password/page.js
+++ Client/src/app/forget-password/page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ForgotPassword from '../Components/ForgotPassword'
 const ForgetPassword = () => {
   return (
     <div>ForgetPassword</div>
