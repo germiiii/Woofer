15cfba71e1ac6d7d6bef317d535bc5b18897d326
LucasZibaitis <zibaitislucas@gmail.com>
2023-11-21 20:06:51 -0300

    Registration Form alert y mensajito en ingles

--- Client/src/app/Components/RegisterForm.jsx
+++ Client/src/app/Components/RegisterForm.jsx
@@ -50,6 +50,7 @@ export default function RegisterForm() {
       );
       setFormSent(true);
     } catch (e) {
+      window.alert("Registration failed.");
       console.log("register sin exito");
     }
   };
@@ -58,8 +59,7 @@ export default function RegisterForm() {
     <div className="bg-white text-black p-6 max-w-md mx-auto mt-10 rounded-md shadow-md">
       {formSent ? (
         <div className="text-green-500 mb-4">
-          Registro exitoso, compruebe su casilla de mensajes para validar su
-          cuenta.
+          Registration successful! Check your inbox for an email confirmation.
           <button
             className="bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
             onClick={() => {
