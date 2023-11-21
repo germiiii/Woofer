19bebe192a27609f19cf94c4342f7182922cc38d
germiiii <germi560@gmail.com>
2023-11-21 17:19:50 -0300

    fix comm issues btw B n F

--- Server/Database/models/dog.js
+++ Server/Database/models/dog.js
@@ -27,7 +27,7 @@ module.exports = sequelize => {
         allowNull: true,
       },
       size: {
-        type: DataTypes.ENUM('small', 'medium', 'large'),
+        type: DataTypes.ENUM('Small', 'Medium', 'Large'),
         allowNull: false,
       },
       is_active: {
