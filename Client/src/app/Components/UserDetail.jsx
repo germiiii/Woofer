8ddd4ae5fa73d3e9ca359272e31e1d06e433931f
MVCorreas <correasmv@gmail.com>
2023-11-22 19:31:48 -0300

    Before Merging to develop to test userdetail path

--- Client/src/app/Components/UserDetail.jsx
+++ Client/src/app/Components/UserDetail.jsx
@@ -1,36 +1,61 @@
-// pages/userDetails.js
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Image from 'next/image';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUserDetails, setUserDetails, selectUserDetails } from '../../redux/features/userDetailSlice';
 
-const UserDetail = ({ users }) => { //Whatever you return from getServerSideProps as props will be available as props in your component
+const UserDetail = ({ userId }) => {
+  const dispatch = useDispatch();
+  const userDetails = useSelector(selectUserDetails);
+
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchUserDetails(userId));
+    }
+  }, [userId, dispatch]);
+
+
   return (
     <div>
-      <h1>User Details</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <p>Name: {user.name}</p>
-            <p>Surname: {user.lastName}</p>
-            <p>Email: {user.email}</p>
-            <p>Address: {user.address}</p>
-            <p>Rating: {user.rating}</p>
-            <p>Reviews: {user.reviews}</p>
-          </li>
-        ))}
-      </ul>
+      <h2>User Profile</h2>
+      {userDetails && (
+        <div>
+          <p>Name: {userDetails.name}</p>
+          <p>Last Name: {userDetails.lastName}</p>
+          <p>Username: {userDetails.username}</p>
+          <p>Email: {userDetails.email}</p>
+          <p>Address: {userDetails.address}</p>
+          {/* Display other user details similarly */}
+          {userDetails.isOwner ? (
+            <div>
+              <h3>Owner Details</h3>
+              <p>Dog Count: {userDetails.dog_count}</p>
+              {userDetails.dog_count >= 1 ? (
+                userDetails.dogs && userDetails.dogs.map((dog) => (
+                  <div key={dog.id}>
+                    <p>Name: {dog.name}</p>
+                    <p>Breed: {dog.breed}</p>
+                    <p>Size: {dog.size}</p>
+                    <p>Age: {dog.age}</p>
+                    <p>Image: {dog.image}</p>
+                  </div>
+                ))
+              ) : (
+                <p>No dogs have been added to your profile.</p>
+              )}
+            </div>
+          ) : (
+            <div>
+              <h3>Walker Details</h3>
+              <p>Dog Capacity: {userDetails.dog_capacity}</p>
+              <p>Is Available: {userDetails.is_available ? 'Yes' : 'No'}</p>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
+  
 };
-
-export async function getServerSideProps() {
- 
-  const res = await axios.get('http://localhost:3001/users'); 
-
-  return {
-    props: {
-      users: res.data || [], 
-    },
-  };
-}
 
 export default UserDetail;
