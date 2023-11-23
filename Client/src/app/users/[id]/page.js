7597750d304e21a90984373bf750ee45db3a8865
MVCorreas <correasmv@gmail.com>
2023-11-23 13:14:36 -0300

    Registration not working

---
+++ Client/src/app/users/[id]/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState } from "react";
+import UserDetail from "../Components/UserDetail";
+
+export default function UserDetailPage() {
+  return (
+    <div>
+       <h1>Detail</h1>
+    </div>
+  );
+}
