48810d8f8d815d01170d1a2501ddacc6e444a2fc
LucasZibaitis <zibaitislucas@gmail.com>
2023-11-23 17:54:09 -0300

    Corrección de address del register

--- Client/src/app/Components/RegisterForm.jsx
+++ Client/src/app/Components/RegisterForm.jsx
@@ -12,7 +12,7 @@ export default function RegisterForm() {
   const [userData, setUserData] = useState({
     name: "",
     lastName: "",
-    adress: "",
+    address: "",
     username: "",
     email: "",
     password: "",
@@ -58,97 +58,139 @@ export default function RegisterForm() {
 
   return (
     <div className="max-w-md mx-auto mt-10 p-4 bg-indigo-200 rounded shadow-md">
-     <div className="flex justify-center">
-    {formSent ? (
-      <div className="text-green-500 mb-4">
-       Registration successful! Check your inbox for a confirmation email.
-        <button
-          className="bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
-          onClick={() => {
-            router.push("/login");
-          }}
-        >
-          LogIn
-        </button>
-        </div>
-      ) : (
-        <form onSubmit={handleRegister} method="post">
-          <div className="flex justify-center">
-        <Image
-          src="/ISOWoofer.png"
-          alt="logo"
-          width={200}
-          height={90}
-          className="mx-auto"
-        />
-      </div>
-          <h1  className="text-2xl text-indigo-900 font-extrabold mb-4">REGISTER</h1>
-          <label className="block mb-2">
-            Your name
-            <input type="text" name="name" placeholder='Enter your name...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"/>
-          </label>
-          <br />
-          <label className="block mb-2">
-            Your last name
-            <input type="text" name="lastName" placeholder='Enter your last name...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500" />
-          </label>
-          <br />
-          <label className="block mb-2">
-            Your username
-            <input type="text" name="username" placeholder='Enter your username...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500" />
-          </label>
-          <br />
-          <label className="block mb-2">
-            Your email
-            <input type="email" name="email" placeholder='Enter your email...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"/>
-          </label>
-          <br />
-          <label className="block mb-2">
-            Your address
-            <input type="text" name="adress" placeholder='Enter your address...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"/>
-          </label>
-          <br />
-          <label className="block mb-2">
-            Your password
-            <input type="password" name="password" placeholder='Enter your password...' onChange={handleChange} className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"/>
-          </label>
-          <br />
-          <label className="block mb-2">
-            Profile Picture
-            <img src={userData.image ? userData.image : '/Profile.jpeg'} alt="" height="100px" width="100px" />
-            
-            <input
-              type="file"
-              name="image"
-              accept="image/*"
-              onChange={handleChange}
-              ref={fileInputRef}
-              className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
-            />
-          </label>
-          <br />
-          <label className="block mb-2">
-            Type of Woofer
-            <select
-              name="isWalker"
-              onChange={handleChange}
-              value={userData.isWalker}
-              className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+      <div className="flex justify-center">
+        {formSent ? (
+          <div className="text-green-500 mb-4">
+            Registration successful! Check your inbox for a confirmation email.
+            <button
+              className="bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
+              onClick={() => {
+                router.push("/login");
+              }}
             >
-              <option value="false">Owner</option>
-              <option value="true">Walker</option>
-            </select>
-          </label>
-          <br />
-          <button
-            type="submit"
-            className="bg-indigo-900 text-white py-2 px-4 rounded focus:outline-none hover:bg-blue-600"
-          >
-            Sign Up
-          </button>
-        </form>
-      )}
-    </div>
+              LogIn
+            </button>
+          </div>
+        ) : (
+          <form onSubmit={handleRegister} method="post">
+            <div className="flex justify-center">
+              <Image
+                src="/ISOWoofer.png"
+                alt="logo"
+                width={200}
+                height={90}
+                className="mx-auto"
+              />
+            </div>
+            <h1 className="text-2xl text-indigo-900 font-extrabold mb-4">
+              REGISTER
+            </h1>
+            <label className="block mb-2">
+              Your name
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your name..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Your last name
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Enter your last name..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Your username
+              <input
+                type="text"
+                name="username"
+                placeholder="Enter your username..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Your email
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter your email..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Your address
+              <input
+                type="text"
+                name="adress"
+                placeholder="Enter your address..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Your password
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password..."
+                onChange={handleChange}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Profile Picture
+              <img
+                src={userData.image ? userData.image : "/Profile.jpeg"}
+                alt=""
+                height="100px"
+                width="100px"
+              />
+              <input
+                type="file"
+                name="image"
+                accept="image/*"
+                onChange={handleChange}
+                ref={fileInputRef}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              />
+            </label>
+            <br />
+            <label className="block mb-2">
+              Type of Woofer
+              <select
+                name="isWalker"
+                onChange={handleChange}
+                value={userData.isWalker}
+                className="border border-gray-300 px-3 py-2 rounded w-full focus:outline-none focus:border-blue-500"
+              >
+                <option value="false">Owner</option>
+                <option value="true">Walker</option>
+              </select>
+            </label>
+            <br />
+            <button
+              type="submit"
+              className="bg-indigo-900 text-white py-2 px-4 rounded focus:outline-none hover:bg-blue-600"
+            >
+              Sign Up
+            </button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
