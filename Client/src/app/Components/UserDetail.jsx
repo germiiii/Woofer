f40bcc78e03cf24559194be8b55899f2382c4801
MVCorreas <correasmv@gmail.com>
2023-11-24 07:41:26 -0300

    Landing showing Nextjs

--- Client/src/app/Components/UserDetail.jsx
+++ Client/src/app/Components/UserDetail.jsx
@@ -5,57 +5,63 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchUserDetails, setUserDetails, selectUserDetails } from '../../redux/features/userDetailSlice';
 
 const UserDetail = ({ userId }) => {
-  const dispatch = useDispatch();
-  const userDetails = useSelector(selectUserDetails);
+  // const dispatch = useDispatch();
+  // const userDetails = useSelector(selectUserDetails);
 
-  useEffect(() => {
-    if (userId) {
-      dispatch(fetchUserDetails(userId));
-    }
-  }, [userId, dispatch]);
+  // useEffect(() => {
+  //   if (userId) {
+  //     dispatch(fetchUserDetails(userId));
+  //   }
+  // }, [userId, dispatch]);
 
 
   return (
     <div>
       <h2>User Profile</h2>
-      {userDetails && (
-        <div>
-          <p>Name: {userDetails.name}</p>
-          <p>Last Name: {userDetails.lastName}</p>
-          <p>Username: {userDetails.username}</p>
-          <p>Email: {userDetails.email}</p>
-          <p>Address: {userDetails.address}</p>
-          {/* Display other user details similarly */}
-          {userDetails.isOwner ? (
-            <div>
-              <h3>Owner Details</h3>
-              <p>Dog Count: {userDetails.dog_count}</p>
-              {userDetails.dog_count >= 1 ? (
-                userDetails.dogs && userDetails.dogs.map((dog) => (
-                  <div key={dog.id}>
-                    <p>Name: {dog.name}</p>
-                    <p>Breed: {dog.breed}</p>
-                    <p>Size: {dog.size}</p>
-                    <p>Age: {dog.age}</p>
-                    <p>Image: {dog.image}</p>
-                  </div>
-                ))
-              ) : (
-                <p>No dogs have been added to your profile.</p>
-              )}
-            </div>
-          ) : (
-            <div>
-              <h3>Walker Details</h3>
-              <p>Dog Capacity: {userDetails.dog_capacity}</p>
-              <p>Is Available: {userDetails.is_available ? 'Yes' : 'No'}</p>
-            </div>
-          )}
-        </div>
-      )}
     </div>
   );
   
 };
 
 export default UserDetail;
+
+// return (
+//   <div>
+//     <h2>User Profile</h2>
+//     {userDetails && (
+//       <div>
+//         <p>Name: {userDetails.name}</p>
+//         <p>Last Name: {userDetails.lastName}</p>
+//         <p>Username: {userDetails.username}</p>
+//         <p>Email: {userDetails.email}</p>
+//         <p>Address: {userDetails.address}</p>
+//         {/* Display other user details similarly */}
+//         {userDetails.isOwner ? (
+//           <div>
+//             <h3>Owner Details</h3>
+//             <p>Dog Count: {userDetails.dog_count}</p>
+//             {userDetails.dog_count >= 1 ? (
+//               userDetails.dogs && userDetails.dogs.map((dog) => (
+//                 <div key={dog.id}>
+//                   <p>Name: {dog.name}</p>
+//                   <p>Breed: {dog.breed}</p>
+//                   <p>Size: {dog.size}</p>
+//                   <p>Age: {dog.age}</p>
+//                   <p>Image: {dog.image}</p>
+//                 </div>
+//               ))
+//             ) : (
+//               <p>No dogs have been added to your profile.</p>
+//             )}
+//           </div>
+//         ) : (
+//           <div>
+//             <h3>Walker Details</h3>
+//             <p>Dog Capacity: {userDetails.dog_capacity}</p>
+//             <p>Is Available: {userDetails.is_available ? 'Yes' : 'No'}</p>
+//           </div>
+//         )}
+//       </div>
+//     )}
+//   </div>
+// );
--- Client/src/app/users/page.js
+++ Client/src/app/users/page.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { useRouter } from "next/router";
-import UserDetail from "../../Components/UserDetail";
+import React from 'react';
+// import { useRouter } from 'next/router';
+// import UserDetail from '../../Components/UserDetail';
 
-const UserDetailPage = () => {
-  const router = useRouter();
-  const { id } = router.query; // Fetch the user ID from the URL query params
+const UserDetails = () => {
+  // const router = useRouter();
+  // const { id } = router.query; // Fetch the user ID from the URL query params
 
   return (
     <div>
       <h1>User Details</h1>
-      {id && <UserDetail userId={id} />}
+      {/* id && <UserDetail userId={id} /> */}
     </div>
   );
 };
 
-export default UserDetailPage;
+export default UserDetails;
