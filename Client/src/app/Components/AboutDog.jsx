16205585c39c23bbfbee2bef48f997c2726a6b97
germiiii <germi560@gmail.com>
2023-11-28 08:38:34 -0300

    ?

--- Client/src/app/Components/AboutDog.jsx
+++ Client/src/app/Components/AboutDog.jsx
@@ -1,7 +1,7 @@
 /* @jsxImportSource client */
 import React, { useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const AboutDog = () => {
   const [tab, setTab] = useState("skills");
