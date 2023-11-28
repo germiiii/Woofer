13208c46b5ea7796770a09229919e6a1cb51ef5b
LucasZibaitis <128875825+LucasZibaitis@users.noreply.github.com>
2023-11-28 14:50:26 -0300

    Update page.js

--- Client/src/app/login/page.js
+++ Client/src/app/login/page.js
@@ -1,5 +1,5 @@
 "use client"
-import LoginForm from '../Components/LoginForm';
+import LoginForm from '../Components/LoginForm.jsx';
 import { useDispatch } from 'react-redux'
 
 export default function LoginPage() {
