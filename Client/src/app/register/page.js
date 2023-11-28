02efdc004d1241bca4b3782df770fa2ad4f39f1a
LucasZibaitis <128875825+LucasZibaitis@users.noreply.github.com>
2023-11-28 16:03:16 -0300

    Update page.js

--- Client/src/app/register/page.js
+++ Client/src/app/register/page.js
@@ -1,10 +1,11 @@
-"use client";
-import React, { useState } from "react";
-// import RegisterForm from "../Components/RegisterForm.jsx";
+// "use client";
+import React from "react";
+import RegisterForm from "../Components/RegisterForm.jsx";
 
 export default function RegisterPage() {
   return (
     <div>
+    <RegisterForm />
     </div>
   );
 }
