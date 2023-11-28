311014fc4e94fe1241a355b53b68107791b9e750
germiiii <germi560@gmail.com>
2023-11-28 18:27:42 -0300

    9

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import CounterReducer from '../features/counterSlice'
-import userDetailSlice from '../features/userDetailSlice';
+//import CounterReducer from './features/counterSlice'
+import userDetailSlice from './features/userDetailSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: CounterReducer, //counter is the name given at counterSlice
+    //counter: CounterReducer, //counter is the name given at counterSlice
     userDetail: userDetailSlice
   },
 })
