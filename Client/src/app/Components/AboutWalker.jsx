4aee00efba0a18473f97720d63d09ab05f23b5df
MVCorreas <correasmv@gmail.com>
2023-11-27 22:18:59 -0300

    Settings form fixed, Landing with functional buttons, user-id working

--- Client/src/app/Components/AboutWalker.jsx
+++ Client/src/app/Components/AboutWalker.jsx
@@ -1,10 +1,12 @@
 "use client"
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const AboutWalker = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -16,12 +18,15 @@ const AboutWalker = () => {
     <section  id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full ">
-          <h2 className="text-4xl font-bold text-[#1e1b4b] mb-4">About Woofer</h2>
+          <h2 className="text-4xl font-bold text-[#1e1b4b] mb-4">Want to wuff some air?</h2>
           <p className="text-base lg:text-lg">
             Want to add more clients to your combo walk? Register to offer dog owners around your location the possibility to walk their dogs on-the-spot. Just activate your account and start receiving walk requests!
           </p>
           <div className="flex items-center justify-center mt-6">
-            <button  className="px-4 py-3 rounded-full bg-[#29235c] text-white hover:bg-amber-400 hover:text-black border mt-3 lg:mt-0 mr-5">
+            <button  className="px-4 py-3 rounded-full bg-[#29235c] text-white hover:bg-amber-400 hover:text-black border mt-3 lg:mt-0 mr-5" 
+             onClick={() => {
+              router.push("/register");
+            }}>
               <span >Register as walker</span>
             </button>
           </div>
