19a44234bbb09a3ff1e5fb1a5c46f6401f12325f
germiiii <germi560@gmail.com>
2023-11-28 08:05:29 -0300

    about dogg fixed

--- Client/src/app/Components/AboutDog.jsx
+++ Client/src/app/Components/AboutDog.jsx
@@ -1,4 +1,4 @@
-// Ensure you have the necessary imports
+/* @jsxImportSource client */
 import React, { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router"; // Change from "next/navigation" to "next/router"
