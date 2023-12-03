18972fd856eeba6587db0e6acec2d080024cdf84
MVCorreas <correasmv@gmail.com>
2023-12-03 07:25:39 -0300

    AccessToken en useEffect agregado

--- Client/src/api/checkout.js
+++ Client/src/api/checkout.js
@@ -1,42 +1,106 @@
-import paypal from '@paypal/checkout-server-sdk'
-import { NextResponse } from 'next/server'
+// pages/api/paypal.js
+import paypal from '@paypal/checkout-server-sdk';
+import fetch from 'node-fetch';
 
-const clientId = 'AahLJYwOxpB8rxP5MCqopNDEgLYJFoaNOxwA0BmVEEzeJCj9yYml78eYMLTpAjVAjYS4svveNkYIXGeF'
-const clientSecret = 'EOkdKdHrKXKUZVvjjKJMZGB37imq1nQhcfZZS70Ce8x5GWDnlGhv49g8tY17xj9xAr1_qt_-RSMiq38k'
+const clientId = 'YOUR_CLIENT_ID';
+const clientSecret = 'YOUR_CLIENT_SECRET';
+const tokenUrl = 'https://api-m.sandbox.paypal.com/v1/oauth2/token';
 
-const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret)
-const client = new paypal.core.PayPalHttpClient(environment)
+export default async function CheckoutHandler(req, res) {
+  try {
+    // Get the access token
+    const auth = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+    const tokenResponse = await fetch(tokenUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${auth}`,
+      },
+      body: 'grant_type=client_credentials',
+    });
 
-export async function POST() {
+    const tokenData = await tokenResponse.json();
+    const accessToken = tokenData.access_token;
+
+    // Use the obtained access token for PayPal API requests
+    const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret, accessToken);
+    const client = new paypal.core.PayPalHttpClient(environment);
+
+    // Create a PayPal order
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.requestBody({
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: '30.00',
+          },
+          items: [
+            {
+              name: '15 minute Premium dog walk',
+              description: 'Woofers will devote their full attention to walk your furry companion privately for 15 minutes.',
+              quantity: '1',
+            },
+          ],
+        },
+      ],
+    });
+
+    const response = await client.execute(request);
+    console.log(response);
+
+    res.status(200).json({
+      id: response.result.id,
+      // Additional data if needed
+    });
+  } catch (error) {
+    console.error('PayPal API error:', error);
+    res.status(500).json({ error: 'Failed to create PayPal order' });
+  }
+}
+
+
+
+// import paypal from '@paypal/checkout-server-sdk'
+// import { NextResponse } from 'next/server'
+
+// const clientId = 'AahLJYwOxpB8rxP5MCqopNDEgLYJFoaNOxwA0BmVEEzeJCj9yYml78eYMLTpAjVAjYS4svveNkYIXGeF'
+// const clientSecret = 'EOkdKdHrKXKUZVvjjKJMZGB37imq1nQhcfZZS70Ce8x5GWDnlGhv49g8tY17xj9xAr1_qt_-RSMiq38k'
+
+// const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret)
+// const client = new paypal.core.PayPalHttpClient(environment)
+
+// export async function POST() {
  
-        const request = new paypal.orders.OrdersCreateRequest()
+//         const request = new paypal.orders.OrdersCreateRequest()
 
-        request.requestBody({
-            intent: 'CAPTURE',
-            purchase_units: [
-                {
-                    amount: {
-                        currency_code: 'USD',
-                        value: '30.00',
-                    },
-                    items: [
-                        {
-                            name: '15 minute Premium dog walk',
-                            description: 'Woofers will devote their full attention to walk your furry companion privately for 15 minutes.',
-                            quantity: '1'
-                        }
-                    ]
-                }
-            ]
-        })
+//         request.requestBody({
+//             intent: 'CAPTURE',
+//             purchase_units: [
+//                 {
+//                     amount: {
+//                         currency_code: 'USD',
+//                         value: '30.00',
+//                     },
+//                     items: [
+//                         {
+//                             name: '15 minute Premium dog walk',
+//                             description: 'Woofers will devote their full attention to walk your furry companion privately for 15 minutes.',
+//                             quantity: '1'
+//                         }
+//                     ]
+//                 }
+//             ]
+//         })
 
-        const response = await client.execute(request)
-        console.log(response)
+//         const response = await client.execute(request)
+//         console.log(response)
 
-      return NextResponse.json({
-        id: response.result.id
-      })
+//       return NextResponse.json({
+//         id: response.result.id
+//       })
     
 
    
-}
+// }
--- Client/src/app/Components/PayPal.jsx
+++ Client/src/app/Components/PayPal.jsx
@@ -25,47 +25,51 @@ const PayPal = () => {
         }}
         >
           <PayPalButtons
-  style={{ layout: "vertical", color: "gold" }}
-  createOrder={async (data, actions) => {
-    try {
-      const res = await fetch("https://api-m.sandbox.paypal.com/v2/checkout/orders", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          // Include your PayPal access token here in the Authorization header
-          "Authorization": `Bearer A21AALvQ1csm1T6QDImeYcQKhCo4bpIRyRPZBb0rwXcOoGKskk1mIMAJqbKUwGYCtaXesvggLHtZ1-yYh-_SMAyErHmkc7PDw`,
-        },
-        body: JSON.stringify({
-          intent: "CAPTURE",
-          purchase_units: [
-            {
-              amount: {
-                currency_code: "USD",
-                value: "30.00",
-              },
-            },
-          ],
-        }),
-      });
-      
-      const order = await res.json();
-      console.log(order);
-      return order.id; // Make sure the order ID is returned correctly
-    } catch (error) {
-      console.error("Error creating PayPal order:", error);
-      // Handle errors here
-    }
-  }}
-  onCancel={(data) => {
-    console.log("Cancelled:", data);
-  }}
-  onApprove={(data, actions) => {
-    console.log("Approved:", data);
-    alert('Payment successful')
-    router.push('/home')
-    actions.order.capture();
-  }}
-/>
+              style={{ layout: "vertical", color: "gold" }}
+              
+              createOrder={async (data, actions) => {
+                try {
+                  const res = await fetch("https://api-m.sandbox.paypal.com/v2/checkout/orders", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      //PayPal access token  after Bearer
+                      "Authorization": `Bearer A21AAI1HPNYh2QtbKqXwumBxrR-n1Q72BZ47G1aKe4Wauy70fITLsHkDc67PVEKCNeLPKfjDynnMAYuPM3WduRev044wxzxjw`,
+                    },
+                    body: JSON.stringify({
+                      intent: "CAPTURE",
+                      purchase_units: [
+                        {
+                          amount: {
+                            currency_code: "USD",
+                            value: "30.00",
+                          },
+                        },
+                      ],
+                    }),
+                  });
+                  
+                  const order = await res.json();
+                  console.log(order);
+                  return order.id; 
+                } catch (error) {
+                  console.error("Error creating PayPal order:", error);
+    
+              }
+            }}
+            onCancel={(data) => {
+              console.log("Cancelled:", data);
+            }}
+            onApprove={(data, actions) => {
+              console.log("Approved:", data);
+              actions.order.capture();
+              alert('Payment successful')
+              setTimeout(() => {
+                router.push('/home');
+              }, 3000); 
+             
+            }}
+          />
 
         </PayPalScriptProvider>
       </div>
@@ -75,88 +79,3 @@ const PayPal = () => {
 };
 
 export default PayPal;
-// "use client"
-// import { useEffect } from 'react';
-
-// const PayPal = () => {
-//   useEffect(() => {
-//     const loadPayPalScript = () => {
-//       const script = document.createElement('script');
-//       script.src = 'https://www.paypal.com/sdk/js?client-id=AahLJYwOxpB8rxP5MCqopNDEgLYJFoaNOxwA0BmVEEzeJCj9yYml78eYMLTpAjVAjYS4svveNkYIXGeF';
-//       script.async = true;
-//       script.onload = initializePayPalButtons;
-//       document.body.appendChild(script);
-//     };
-
-//     const initializePayPalButtons = () => {
-//       const createOrder = async () => {
-//         try {
-//           const response = await fetch('https://api-m.sandbox.paypal.com/v2/checkout/orders', {
-//             method: 'POST',
-//             headers: {
-//               'Content-Type': 'application/json',
-//               'Authorization': `Bearer ${accessToken}`, // Replace with your actual access token
-//             },
-//             body: JSON.stringify({
-//               intent: 'CAPTURE',
-//               purchase_units: [
-//                 {
-//                   amount: {
-//                     currency_code: 'USD',
-//                     value: '100.00', // Replace with the total amount of the order
-//                   },
-//                 },
-//               ],
-//             }),
-//           });
-      
-//           const orderData = await response.json();
-//           console.log('Order Data:', orderData); // Check the structure and contents
-
-//           if (orderData.id) {
-//             return orderData.id;
-//           } else {
-//             // Handle error cases when creating the order
-//           }
-//         } catch (error) {
-//           console.error('Error creating order:', error);
-//           // Handle errors here
-//         }
-//       };
-      
-      
-//       const onApprove = async (data, actions) => {
-//         try {
-//           // Logic for processing the approved payment
-//           console.log(data);
-//         } catch (error) {
-//           console.error('Error on approve:', error);
-//           // Handle errors in the approval process
-//         }
-//       };
-
-//       const PayPalButtonContainer = window.paypal.Buttons({
-//         createOrder: createOrder,
-//         onApprove: onApprove,
-//       });
-
-//       PayPalButtonContainer.render('#paypal-button-container');
-
-//       const resultMessage = (message) => {
-//         const container = document.querySelector('#result-message');
-//         container.innerHTML = message;
-//       };
-//     };
-
-//     loadPayPalScript();
-//   }, []);
-
-//   return (
-//     <div>
-//       <div id="paypal-button-container"></div>
-//       <div id="result-message"></div>
-//     </div>
-//   );
-// };
-
-// export default PayPal;
