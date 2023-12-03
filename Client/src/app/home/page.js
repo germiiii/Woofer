7f9267aa3910d16a1624ed391960f204eed9eea3
LucasZibaitis <128875825+LucasZibaitis@users.noreply.github.com>
2023-12-03 19:13:18 -0300

    Update page.js
    endpoint home

--- Client/src/app/home/page.js
+++ Client/src/app/home/page.js
@@ -18,7 +18,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/users");
+        const response = await axios.get("https://woofer-server-nsjo.onrender.com/users");
       } catch (error) {
         console.error("Error fetching data:", error);
       }
