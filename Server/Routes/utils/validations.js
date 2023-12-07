00c31385c9d547ffa5b2a78569f1b9c2e7985f9c
RRodriguezViegas <ramiirv26@gmail.com>
2023-12-07 07:39:42 -0300

    added alphanumeric validation function

--- Server/Routes/utils/validations.js
+++ Server/Routes/utils/validations.js
@@ -1,5 +1,6 @@
 const regexNotSpecialNorNumber = /^[a-zA-Z,'.\-\s]*$/i;
 const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const alphanumeric = /^[A-Za-z0-9]+$/g;
 
 const validateSpecialAndNumber = str => {
   if (regexNotSpecialNorNumber.test(str)) {
@@ -17,7 +18,16 @@ const validateEmail = email => {
   }
 };
 
+const validateAlphanumeric = str => {
+  if (alphanumeric.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 module.exports = {
   validateSpecialAndNumber,
   validateEmail,
+  validateAlphanumeric,
 };
