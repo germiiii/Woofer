6cc6b2597a2c0923aaad78b111a6670c7eeccaa2
MVCorreas <correasmv@gmail.com>
2023-12-07 17:18:48 -0300

    Home funcitonal with filters of 1 walktype and paypal correctly redirected

--- Client/src/app/Components/WalkerCard.jsx
+++ Client/src/app/Components/WalkerCard.jsx
@@ -69,23 +69,23 @@ const WalkerCard = (props) => {
     let serviceId = "";
   
     if (props.walkDuration.includes("15") && props.dogCapacity === 1) {
-      serviceId = "6562eca1-c96e-490e-98e8-dbac5b73c76f";
+      serviceId = "1";
     } else if (props.walkDuration.includes("30") && props.dogCapacity === 1) {
-      serviceId = "b2b40736-b68c-46a1-9bd4-a7c075bfd0e2";
+      serviceId = "2";
     } else if (props.walkDuration.includes("60") && props.dogCapacity === 1) {
-      serviceId = "6095c3d3-1eff-4346-abb1-94805f809004";
+      serviceId = "3";
     } else if (props.walkDuration.includes("15") && props.dogCapacity > 2 && props.dogCapacity < 5) {
-      serviceId = "4e38244e-63e4-4f38-b44c-d4ce40125a73";
+      serviceId = "4";
     } else if (props.walkDuration.includes("30") && props.dogCapacity > 2 && props.dogCapacity < 5) {
-      serviceId = "774e50ac-00fa-4e4f-8ff3-51172285f540";
+      serviceId = "5";
     } else if (props.walkDuration.includes("60") && props.dogCapacity > 2 && props.dogCapacity < 5) {
-      serviceId = "e941d376-ae18-4586-9a0f-02b3fae8e846";
+      serviceId = "6";
     } else if (props.walkDuration.includes("15") && props.dogCapacity > 5) {
-      serviceId = "b8311648-213e-4974-bf7d-e73147b910b0";
+      serviceId = "7";
     } else if (props.walkDuration.includes("30") && props.dogCapacity > 5) {
-      serviceId = "91e3adf6-6efc-4298-bec2-608a90b11823";
+      serviceId = "8";
     } else if (props.walkDuration.includes("60") && props.dogCapacity > 5) {
-      serviceId = "cfdfe8a4-393b-4e69-883d-ea73950f8063";
+      serviceId = "9";
     }
   
     if (serviceId) {
