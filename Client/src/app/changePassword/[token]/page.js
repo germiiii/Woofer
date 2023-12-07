6da0ac3205746c2a81e37b383bb0fd1cc68a7419
LucasZibaitis <128875825+LucasZibaitis@users.noreply.github.com>
2023-12-07 19:01:04 -0300

    Update page.js

--- Client/src/app/changePassword/[token]/page.js
+++ Client/src/app/changePassword/[token]/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import ChangePassword from '../../Components/changePassword';
+import ChangePassword from '../../Components/ChangePassword';
 
 const ChangePasswordPage = () => {
   return (
