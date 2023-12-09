d7e223773d8aa68ade489a51bf5789f2f63136bc
RRodriguezViegas <ramiirv26@gmail.com>
2023-12-09 08:04:08 -0300

    fix /g

--- Server/Routes/utils/validations.js
+++ Server/Routes/utils/validations.js
@@ -1,7 +1,7 @@
 const regexNotSpecialNorNumber = /^[a-zA-Z,'.\-\s]*$/i;
 const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const alphanumericNoSpaces = /^[A-Za-z0-9]+$/g;
-const alphanumeric = /^[A-Za-z0-9]+$/g;
+const alphanumericNoSpaces = /^[A-Za-z0-9]+$/;
+const alphanumeric = /^[A-Za-z0-9\s]+$/;
 
 const validateSpecialAndNumber = str => {
   if (regexNotSpecialNorNumber.test(str)) {
